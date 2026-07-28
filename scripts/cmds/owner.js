890422ba7f425be02d951d35b8305c5cbcfb9712
sksm33 <sksam3318@gmail.com>
2026-07-28 12:53:59 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -6,7 +6,7 @@ module.exports = {
   config: {
     name: "owner",
     version: "1.3.0",
-    author: "亗•𝘔𝘈𝘔𝘜𝘕✿᭄",
+    author: "亗•AʜᴇᴍD's SHISHIR✿᭄",
     role: 0,
     shortDescription: "Owner information with image",
     category: "Information",
@@ -18,17 +18,17 @@ module.exports = {
   onStart: async function ({ api, event }) {
     const ownerText = 
 `╭─ 👑 Oᴡɴᴇʀ Iɴғᴏ 👑 ─╮
-│ 👤 Nᴀᴍᴇ       : 亗•𝘔𝘈𝘔𝘜𝘕✿᭄
+│ 👤 Nᴀᴍᴇ       : 亗• AʜᴇᴍD's SHISHIR ✿᭄
 │🧸 Nɪᴄᴋ       : Vondo
-│ 🎂 Aɢᴇ        : 19+
+│ 🎂 Aɢᴇ        : 17+
 │ 💘 Rᴇʟᴀᴛɪᴏɴ : Sɪɴɢʟᴇ
 │ 🎓 Pʀᴏғᴇssɪᴏɴ : Sᴛᴜᴅᴇɴᴛ
-│ 📚 Eᴅᴜᴄᴀᴛɪᴏɴ : Iɴᴛᴇʀ 2ɴᴅ Yᴇᴀʀ
+│ 📚 Eᴅᴜᴄᴀᴛɪᴏɴ : Iɴᴛᴇʀ 1st Yᴇᴀʀ
 │ 🏡 Lᴏᴄᴀᴛɪᴏɴ : 🄺🄷🅄🄻🄽🄷  
 ├─ 🔗 Cᴏɴᴛᴀᴄᴛ ─╮
-│ 📘 Facebook  : https://www.facebook.com/share/171Y441F7H/
-│ 💬 Messenger: m.me/61558931578859
-│ 📞 WhatsApp  : wa.me/01830981279
+│ 📘 Facebook  : Yuzaki 
+│ 💬 Messenger: m.me/ 61570871885492
+│ 📞 WhatsApp  : wa.me/017493__26
 ╰────────────────╯`;
 
     const cacheDir = path.join(__dirname, "cache");
